22ab4fc63d29a31c55d6c3d49ce8de807261896f
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 10:56:29 +0100

    custom print

--- test/print_custom.c
+++ test/print_custom.c
@@ -10,6 +10,7 @@
  * if a flag is passed to _printf
  * Return: number of char printed
  */
+
 int print_bigS(va_list l, flags_t *f)
 {
 	int i, count = 0;
@@ -44,6 +45,7 @@ int print_bigS(va_list l, flags_t *f)
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
+
 int print_rev(va_list l, flags_t *f)
 {
 	int i = 0, j;
@@ -69,6 +71,7 @@ int print_rev(va_list l, flags_t *f)
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
+
 int print_rot13(va_list l, flags_t *f)
 {
 	int i, j;
@@ -100,6 +103,7 @@ int print_rot13(va_list l, flags_t *f)
  * @f: pointer to the struct flags in which we turn the flags on
  * Return: number of char printed
  */
+
 int print_percent(va_list l, flags_t *f)
 {
 	(void)l;
