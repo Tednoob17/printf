136350b77073bef33037bb5d77e06a57069aa422
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 10:25:47 +0100

    fnct chars

--- main.h
+++ main.h
@@ -51,16 +51,21 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
+
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
+
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
