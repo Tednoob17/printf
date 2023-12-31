ed4815184ff59637fa836ea5b469ecf4f1fa187c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 10:30:23 +0100

    utils

--- main.h
+++ main.h
@@ -115,10 +115,13 @@ int print_rot13string(va_list types, char buffer[],
 
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
+
 int write_number(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
+
 int write_num(int ind, char bff[], int flags, int width, int precision,
 	int length, char padd, char extra_c);
+
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
 
