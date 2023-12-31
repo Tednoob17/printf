72f00c9eb08e694a9f20fb6ee376ca87b4aa093e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 10:36:56 +0100

    rot13 precision

--- functions2.c
+++ functions2.c
@@ -137,6 +137,7 @@ int print_reverse(va_list types, char buffer[],
 	}
 	return (count);
 }
+
 /************************* PRINT A STRING IN ROT13 *************************/
 /**
  * print_rot13string - Print a string in rot13.
@@ -148,6 +149,7 @@ int print_reverse(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
+
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
