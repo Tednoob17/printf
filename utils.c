f33c0ecca6eec26724d532f3280451d2457d9667
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 10:47:25 +0100

    utils

--- utils.c
+++ utils.c
@@ -6,6 +6,7 @@
  *
  * Return: 1 if c is printable, 0 otherwise
  */
+
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
@@ -21,6 +22,7 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
+
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
@@ -43,6 +45,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
+
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -58,6 +61,7 @@ int is_digit(char c)
  *
  * Return: Casted value of num
  */
+
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
@@ -75,6 +79,7 @@ long int convert_size_number(long int num, int size)
  *
  * Return: Casted value of num
  */
+
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
